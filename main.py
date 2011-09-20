24baa068248caa80660e0f11a147ea8a9bda2e7c
Adam Lilienfeldt <lilienfeldt.adam@gmail.com>
2011-09-20 18:25:12 +0200

    Fixed all bugs, everything now works

--- main.py
+++ main.py
@@ -1,74 +1,51 @@
 import os
 
 print ("Welcome to Adam Lilienfeldt's All-install program. This will only work on Linux distributions that supports apt-get")
-print ("Make sure that you run this script as root (sudo)")
+print ("Make sure that you run this program as root (sudo)")
 
 
+###Regular programs###
 
-os.system ("apt-get install basket")
-os.system ("y")
+os.system ("apt-get install basket -y")
 
-os.system ("apt-get install arduino")
-os.system ("y")
+os.system ("apt-get install arduino -y")
 
-os.system ("apt-get install eagle")
-os.system ("y")
+os.system ("apt-get install eagle -y")
 
-os.system ("apt-get install gresistor")
-os.system ("y")
+os.system ("apt-get install gresistor -y")
 
-os.system ("apt-get install gimp")
-os.system ("y")
+os.system ("apt-get install gimp -y")
 
-os.system ("apt-get install filezilla")
-os.system ("y")
+os.system ("apt-get install filezilla -y")
 
-os.system ("apt-get install thunderbird")
-os.system ("y")
- 
-os.system ("apt-get install git-cola")
-os.system ("y")
+os.system ("apt-get install thunderbird -y")
 
-os.system ("apt-get install eclipse")
-os.system ("y")
+os.system ("apt-get install git-cola -y")
 
-os.system ("apt-get install glade")
-os.system ("y")
- 
-os.system ("apt-get install audacity")
-os.system ("y")
+os.system ("apt-get install eclipse -y")
 
-os.system ("apt-get install vlc")
-os.system ("y")
+os.system ("apt-get install glade -y")
 
-os.system("apt-get install cheese")
-os.system("y")
+os.system ("apt-get install audacity -y")
 
-os.system ("apt-get install gtk-recordmydesktop")
-os.system ("y")
+os.system ("apt-get install vlc -y")
 
-os.system ("apt-get install wine")
-os.system ("y")
+os.system("apt-get install cheese -y")
 
-os.system ("apt-get install git")
-os.system ("y")
+os.system ("apt-get install gtk-recordmydesktop -y")
 
-os.system ("apt-get install git-gui")
-os.system ("y")
+os.system ("apt-get install wine -y")
 
-os.system ("apt-get install git-doc")
-os.system ("y")
+os.system ("apt-get install screen -y")
 
-os.system ("apt-get install screen")
-os.system ("y")
+os.system ("apt-get install irssi -y")
 
-os.system ("apt-get install irssi")
-os.system ("y")
+os.system("apt-get install skype -y")
 
-os.system("apt-get install skype")
-os.system("y")
 
-f=open ('/etc/apt/sources.list','w')
+###Sourcelist adding###
+
+f=open ('/etc/apt/sources.list','a')
 f.write ('http://dl.google.com/linux/chrome/deb')
 f.write ('http://dl.google.com/linux/talkplugin/deb')
 f.write ('http://linux.dropbox.com/ubuntu')
@@ -76,32 +53,36 @@ f.close ()
 
 
 os.system ("apt-get install google-chrome-stable")
-os.system ("y")
 
 os.system ("apt-get install google-talkplugin")
-os.system ("y")
 
 os.system ("apt-get install nautilus-dropbox")
-os.system ("y")
+
+
+###Install Notepad++###
 
 os.system ("wget http://download.tuxfamily.org/notepadplus/5.9.3/npp.5.9.3.Installer.exe")
 os.system ("chmod +x npp.5.9.3.Installer.exe")
+os.system("wine start npp.5.9.3.Installer.exe")
+
+###Remove games###
 
 
-os.system("")
-os.system("y")
+os.system("apt-get remove gbrainy -y")
 
-os.system("")
-os.system("y")
+os.system("apt-get remove gnome-mahjongg -y")
 
-os.system("")
-os.system("y")
+os.system("apt-get remove sudoku -y")
 
-os.system("")
-os.system("y")
+os.system("apt-get remove aisleriot -y")
 
-os.system("")
-os.system("y")
 
-os.system("")
-os.system("y")
+###Git installation and github setup###
+
+
+os.system ("apt-get install git -y")
+
+os.system ("apt-get install git-gui -y")
+
+os.system ("apt-get install git-doc -y")
+
